ebf19938a16c9bcb05ec39a71913a9cfdca090c0
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-11-28 04:25:08 +0000

    Update

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -2,92 +2,106 @@ import React, { useState, useEffect } from "react";
 import TodoElement from "./home";
 
 const ToDo = () => {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    useEffect(() => {
-        fetch('https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78')
-            .then(response => {
-                if (!response.ok) {
-                    console.error("Insert task to create user on API");
-                    throw Error(response.statusText);
-                }
-                return response.json();
-            })
-            .then(responseAsJson => {
-                setTodos(responseAsJson);
-            });
-    }, []);
-
-    const markAsCompleted = (elm) => {
-        const updatedTodos = todos.map((todo) =>
-            todo.id === elm.id ? { ...todo, done: true } : todo
-        );
-
-        fetch('https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78', {
-            method: "PUT",
-            body: JSON.stringify(updatedTodos),
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then(res => res.json())
-            .then(() => setTodos(updatedTodos))
-            .catch(error => console.error(error));
-    };
-
-    const deleteAll = () => {
-        fetch('https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78', {
-            method: "PUT",
-            body: JSON.stringify([]),
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then(res => res.json())
-            .then(() => setTodos([]))
-            .catch(error => console.error(error));
-    };
-
-    const deleteCompleted = () => {
-        const updatedTodos = todos.filter((todo) => !todo.done);
-
-        fetch('https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78', {
-            method: "PUT",
-            body: JSON.stringify(updatedTodos),
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then(res => res.json())
-            .then(() => setTodos(updatedTodos))
-            .catch(error => console.error(error));
-    };
-
-    const insertTodo = (text) => {
-        if (text.key === "Enter" && text.target.value !== "") {
-            const newTodo = { label: text.target.value, id: text.target.value + todos.length, done: false };
-
-            fetch('https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78', {
-                method: "PUT",
-                body: JSON.stringify([...todos, newTodo]),
-                headers: { 'Content-Type': 'application/json' }
-            })
-                .then(res => res.json())
-                .then(() => {
-                    setTodos([...todos, newTodo]);
-                    text.target.value = "";
-                })
-                .catch(error => console.error(error));
+  useEffect(() => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78")
+      .then((response) => {
+        if (!response.ok) {
+          console.error("Insert task to create user on API");
+          throw Error(response.statusText);
         }
-    };
+        return response.json();
+      })
+      .then((responseAsJson) => {
+        setTodos(responseAsJson);
+      });
+  }, []);
 
-    return (
-        <>
-            <button onClick={deleteAll} style={{ backgroundColor: 'green', color: '#fff' }}>Borrar</button>
-            <button onClick={deleteCompleted}>Completada</button>
-            <h1 className="d-flex justify-content-center">Tareas</h1>
-            <div className="todo">
-                <input placeholder="Añadir Tarea..." onKeyDown={(e) => insertTodo(e)} />
-                {todos.map((todo) => (
-                    <TodoElement key={todo.id} text={todo.label} click={() => markAsCompleted(todo)} />
-                ))}
-            </div>
-        </>
+  const markAsCompleted = (elm) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === elm.id ? { ...todo, done: true } : todo
     );
+
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78", {
+      method: "PUT",
+      body: JSON.stringify(updatedTodos),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then(() => setTodos(updatedTodos))
+      .catch((error) => console.error(error));
+  };
+
+  const deleteAll = () => {
+    const userIdentifier = { user: "JoseGeek78" };
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78", {
+      method: "PUT",
+      body: JSON.stringify(userIdentifier),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then(() => setTodos([]))
+      .catch((error) => console.error(error));
+  };
+
+  const deleteCompleted = () => {
+    const updatedTodos = todos.filter((todo) => !todo.done);
+
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78", {
+      method: "PUT",
+      body: JSON.stringify(updatedTodos),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then(() => setTodos(updatedTodos))
+      .catch((error) => console.error(error));
+  };
+
+  const insertTodo = (text) => {
+    if (text.key === "Enter" && text.target.value !== "") {
+      const newTodo = {
+        label: text.target.value,
+        id: text.target.value + todos.length,
+        done: false,
+      };
+
+      fetch("https://playground.4geeks.com/apis/fake/todos/user/JoseGeek78", {
+        method: "PUT",
+        body: JSON.stringify([...todos, newTodo]),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then(() => {
+          setTodos([...todos, newTodo]);
+          text.target.value = "";
+        })
+        .catch((error) => console.error(error));
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={deleteAll}
+        style={{ backgroundColor: "green", color: "#fff" }}
+      >
+        Borrar
+      </button>
+      <button onClick={deleteCompleted}>Completada</button>
+      <h1 className="d-flex justify-content-center">Tareas</h1>
+      <div className="todo">
+        <input placeholder="Añadir Tarea..." onKeyDown={(e) => insertTodo(e)} />
+        {todos.map((todo) => (
+          <TodoElement
+            key={todo.id}
+            text={todo.label}
+            click={() => markAsCompleted(todo)}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default ToDo;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 
 const Todolist = (props) => {
-    const [hidden, setHidden] = useState("d-none");
+  const [hidden, setHidden] = useState("d-none");
 
-    return (
-        <span 
-            onMouseOver={() => { setHidden("") }} 
-            onMouseOut={() => { setHidden("d-none") }}
-            onClick={props.click}  
-            style={{ cursor: "pointer" }}  
-        >
-            <p className="fs-3 m-0">{props.text}</p>
-            <i onClick={props.click} className={`fa-solid fa-x fs-6 text-danger ${hidden}`}></i>
-        </span>
-    );
-}
+  return (
+    <span
+      onMouseOver={() => {
+        setHidden("");
+      }}
+      onMouseOut={() => {
+        setHidden("d-none");
+      }}
+      onClick={props.click}
+      style={{ cursor: "pointer" }}
+    >
+      <p className="fs-3 m-0">{props.text}</p>
+      <i
+        onClick={props.click}
+        className={`fa-solid fa-x fs-6 text-danger ${hidden}`}
+      ></i>
+    </span>
+  );
+};
 
 export default Todolist;
